b6205356adf5781fc2df61026511a1aad1a2723f
HP657 <junlee5070@gmail.com>
2024-07-05 23:05:54 +0900

    수정본

--- src/App.js
+++ src/App.js
@@ -4,12 +4,10 @@ import Detail from './routes/Detail';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movie/:id" element={<Detail />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/movie/:id" element={<Detail />} />
+    </Routes>
   );
 }
 
